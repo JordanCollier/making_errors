b106644cf7af1785b57a7dfb3b9d30001a0bd2c5
Jordan Collier <jordanicollier@gmail.com>
2015-03-09 11:33:38 -0600

    add error message for server overcapacity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class AccessDenied < StandardError; end
+
+  rescue_from AccessDenied, with: :record_not_found
+
+  private
+
+    def record_not_found
+      render file: "/public/over_capacity.html", status: 404
+    end
 end
